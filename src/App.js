64927f1e7e71872496681966ca0e65ae788ca679
Muhannad <seefsy@gmail.com>
2022-09-05 17:07:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    i : 0,
+    quotes: []
+  }
+
+  getQuotes = async (e) => {
+    e.preventDefault()
+    const api = await fetch('https://quote-garden.herokuapp.com/api/v3/quotes')
+    let data = await api.json()
+    let quotes = data.data.map( quote => {
+      return (
+        quote.quoteText
+      )
+    })
+    this.setState({
+      i : this.state.i + 1,
+      quotes : quotes
+    })
+    if(this.state.i === 9){
+      this.setState({
+        i : 0
+      })
+    }
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Quotes Generator</h1>
+        {this.state.quotes[this.state.i]}
+        <button onClick={this.getQuotes}>New Quotes</button>
+      </div>
+    )
+  }
 }
 
 export default App;
